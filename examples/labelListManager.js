dc9405d3f5b0677ee4ebb496e8362c22a673be9f
vuong.tran2 <vuong.tran2@sotatek.com>
2020-10-01 15:12:05 +0700

    Add axious

--- examples/labelListManager.js
+++ examples/labelListManager.js
@@ -1,17 +1,80 @@
-var getLabelList = function() {
+const axios = require('axios');
+
+var labelList = [];
+
+var getLabelList = async function() {
   console.log('Get label list');
+  const getUrl = process.env.PACS_HOST + '/api/v1/labeling';
+  await axios
+    .get(getUrl)
+    .then(function(response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function(error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function() {
+      return labelList;
+    });
 };
 
-var addLabelList = function(params) {
+var addLabelList = async function(params) {
   console.log('Add label list');
+
+  const postUrl = process.env.PACS_HOST + '/api/v1/labeling/save';
+  await axios
+    .post(postUrl)
+    .then(function(response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function(error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function() {
+      // always executed
+    });
 };
 
-var updateLabelList = function(params) {
+var updateLabelList = async function(params) {
   console.log('Update label list');
+
+  const patchUrl = process.env.PACS_HOST;
+  await axios
+    .patch(patchUrl)
+    .then(function(response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function(error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function() {
+      // always executed
+    });
 };
 
-var deleteLabelList = function(params) {
+var deleteLabelList = async function(params) {
   console.log('Delete label list');
+
+  const deleteUrl = process.env.PACS_HOST + '/api/v1/labeling';
+  await axios
+    .delete(deleteUrl)
+    .then(function(response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function(error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function() {
+      // always executed
+    });
 };
 
 const labelListManager = {
